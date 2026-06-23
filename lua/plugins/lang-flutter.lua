b92442e106c00445c930b402e6eb5feef949184f
Juan Pablo Gonzalez Lugo <jpglxd@gmail.com>
2026-06-23 12:36:03 -0500

    fix(dart): use lspconfig dartls with FVM path, disable flutter-tools LSP

--- lua/plugins/lang-flutter.lua
+++ lua/plugins/lang-flutter.lua
@@ -1,13 +1,38 @@
--- Flutter/Dart: el extra "lang.dart" ya configura flutter-tools + LSP.
--- Aquí habilitamos FVM para que detecte el SDK del proyecto automáticamente.
+-- Flutter/Dart con FVM
+-- dartls via lspconfig (para LSP: gd, gD, format, etc.)
+-- flutter-tools solo para debugger y comandos :Flutter*
 return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        dartls = {
+          cmd = { vim.fn.expand("~/fvm/default/bin/dart"), "language-server", "--protocol=lsp" },
+          settings = {
+            dart = {
+              completeFunctionCalls = true,
+              showTodos = true,
+              updateImportsOnRename = true,
+              analysisExcludedFolders = {
+                vim.fn.expand("~/fvm/versions"),
+              },
+            },
+          },
+        },
+      },
+    },
+  },
   {
     "akinsho/flutter-tools.nvim",
     opts = {
-      fvm = true, -- detecta .fvmrc / .fvm/ del proyecto y usa ese SDK
+      fvm = true,
       debugger = { enabled = true, run_via_dap = true },
       dev_log = { enabled = true, open_cmd = "tabedit" },
       widget_guides = { enabled = true },
+      lsp = {
+        -- NO dejar que flutter-tools maneje el LSP
+        enabled = false,
+      },
     },
   },
 }
